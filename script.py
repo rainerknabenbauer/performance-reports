8c9001f392af1e21a103a8e71ff89a28f86f8588
Monika Kłopotek <m.klopotek@dental21.de>
2023-02-15 13:43:56 +0100

    improve script checking mobile and desktop results

--- script.py
+++ script.py
@@ -5,21 +5,32 @@ import json
 
 def run():
     print_welcome()
-    performance_report = query_performance()
-    create_report(performance_report)
+    website_url = read_user_input()
+    for strategy in strategies:
+        print("now we are in loop " + strategy)
+        strategy_specific_url = build_full_url(website_url, strategy)
+        performance_report = query_performance(strategy_specific_url)
+        create_report(performance_report)
 
 
 def print_welcome():
     print('Check performance results by providing website URL')
 
 
-def query_performance():
-    website_url = input('enter website URL: ')
-    pagespeed_full_url = pagespeed_url + website_url
-    print(pagespeed_full_url)
-    web_response = requests.get(pagespeed_full_url)
+def read_user_input():
+    return input('enter website URL: ')
 
-    return json.loads(web_response.content)
+
+def build_full_url(website_url, strategy):
+    full_url = pagespeed_base_url + website_url + "&strategy=" + strategy
+    print("url is: " + full_url)
+    return full_url
+
+
+def query_performance(strategy_specific_url):
+    web_response_desktop = requests.get(strategy_specific_url)
+    response = json.loads(web_response_desktop.content)
+    return response
 
 
 def create_report(report):
@@ -33,10 +44,13 @@ def create_report(report):
     print(report["lighthouseResult"]["audits"]["speed-index"]["score"] * 100)
     print("first_contentful_paint")
     print(report["lighthouseResult"]["audits"]["first-contentful-paint"]["score"] * 100)
+    print("first_meaningful_paint")
+    print(report["lighthouseResult"]["audits"]["first-meaningful-paint"]["score"] * 100)
     print("time_to_interactive")
     print(report["lighthouseResult"]["audits"]["interactive"]["score"] * 100)
 
 
 if __name__ == '__main__':
-    pagespeed_url = 'https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url='
+    pagespeed_base_url = 'https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed?url='
+    strategies = ["mobile", "desktop"]
     run()
